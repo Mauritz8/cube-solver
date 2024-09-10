36f4c4c868deebcb74f5267d11b09ebd68744a0b
Mauritz <mauritz.sjodin3@gmail.com>
2024-09-10 21:40:17 +0200

    wip: ability to make moves

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,5 @@
 type color = YELLOW | WHITE | BLUE | RED | GREEN | ORANGE
+type direction = UP | DOWN | RIGHT | LEFT | FRONT | BACK
 
 let side_one_color color = List.init 9 (fun _ -> color)
 
@@ -37,6 +38,51 @@ let cube_to_string cube =
   let bottom = side_string 5 in
   String.concat "\n\n" [ top; front; right; back; left; bottom ]
 
-let () =
-  let cube_string = cube_to_string solved_cube in
-  print_endline cube_string
+let move_up_clockwise cube =
+  let f i x =
+    if i = 0 then List.nth cube 3
+    else if i < 4 then List.nth cube (i - 1)
+    else x
+  in
+  List.mapi f cube
+
+let move_up_counter_clockwise cube =
+  let f i x =
+    if i = 3 then List.nth cube 0
+    else if i < 3 then List.nth cube (i + 1)
+    else x
+  in
+  List.mapi f cube
+
+let move_clockwise cube direction =
+  match direction with
+  | UP -> move_up_clockwise cube
+  | DOWN -> move_up_clockwise cube
+  | RIGHT -> move_up_clockwise cube
+  | LEFT -> move_up_clockwise cube
+  | FRONT -> move_up_clockwise cube
+  | BACK -> move_up_clockwise cube
+
+let move_counter_clockwise cube direction =
+  match direction with
+  | UP -> move_up_counter_clockwise cube
+  | DOWN -> move_up_counter_clockwise cube
+  | RIGHT -> move_up_counter_clockwise cube
+  | LEFT -> move_up_counter_clockwise cube
+  | FRONT -> move_up_counter_clockwise cube
+  | BACK -> move_up_counter_clockwise cube
+
+let move cube direction clockwise =
+  if clockwise then move_clockwise cube direction
+  else move_counter_clockwise cube direction
+
+let cube = solved_cube
+let cube2 = move cube UP true
+let _ = move cube DOWN true
+let _ = move cube RIGHT true
+let _ = move cube LEFT true
+let _ = move cube FRONT true
+let _ = move cube BACK true
+
+(*let () = print_endline (cube_to_string cube)*)
+let () = print_endline (cube_to_string cube2)
