f945019edf88f5c7fd0ef872cc745a2487de700e
Mauritz <mauritz.sjodin3@gmail.com>
2024-09-10 20:36:19 +0200

    feat: print side of cube

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,35 @@
-type sticker = YELLOW | WHITE | BLUE | RED | GREEN | ORANGE
-type corner = sticker * sticker * sticker
-type edge = sticker * sticker
-type top_or_bottom_layer = { corners : corner list; edges : edge list }
-type middle_layer = { corners : corner list; center_stickers : sticker list }
+type color = YELLOW | WHITE | BLUE | RED | GREEN | ORANGE
 
-type rubics_cube = {
-  top_layer : top_or_bottom_layer;
-  middle_layer : middle_layer;
-  bottom_layer : top_or_bottom_layer;
-}
+let side_one_color color = List.init 9 (fun _ -> color)
 
-let () = print_endline "Hello, World!"
+let solved_cube =
+  [
+    side_one_color GREEN;
+    side_one_color ORANGE;
+    side_one_color BLUE;
+    side_one_color RED;
+    side_one_color YELLOW;
+    side_one_color WHITE;
+  ]
+
+let sticker_to_string sticker =
+  match sticker with
+  | YELLOW -> "Y"
+  | WHITE -> "W"
+  | BLUE -> "B"
+  | RED -> "R"
+  | GREEN -> "G"
+  | ORANGE -> "O"
+
+let side_to_string side =
+  let f i x =
+    if i = 2 || i = 5 then String.cat (sticker_to_string x) "\n"
+    else sticker_to_string x
+  in
+  String.concat "" (List.mapi f side)
+
+let cube_to_string cube = side_to_string (List.nth cube 0)
+
+let () =
+  let cube_string = cube_to_string solved_cube in
+  print_endline cube_string
