f16e36245bdb1d1627dbb70c7cf7dde73b85563f
Mauritz <mauritz.sjodin3@gmail.com>
2024-09-10 19:00:13 +0200

    feat: create rubic's cube types

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
+type sticker = YELLOW | WHITE | BLUE | RED | GREEN | ORANGE
+type corner = sticker * sticker * sticker
+type edge = sticker * sticker
+type top_or_bottom_layer = { corners : corner list; edges : edge list }
+type middle_layer = { corners : corner list; center_stickers : sticker list }
+
+type rubics_cube = {
+  top_layer : top_or_bottom_layer;
+  middle_layer : middle_layer;
+  bottom_layer : top_or_bottom_layer;
+}
+
 let () = print_endline "Hello, World!"
