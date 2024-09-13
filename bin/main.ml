c68e033e1e2bb877173df6d72c3ab16f7b161ad4
Mauritz <mauritz.sjodin3@gmail.com>
2024-09-13 10:15:18 +0200

    refactor: change data structure for cube

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,27 @@
 type sticker = YELLOW | WHITE | BLUE | RED | GREEN | ORANGE
 type direction = UP | DOWN | RIGHT | LEFT | FRONT | BACK
+type side = sticker list
+
+type cube = {
+  front : side;
+  right : side;
+  left : side;
+  top : side;
+  bottom : side;
+  back : side;
+}
 
 let side_one_sticker sticker = List.init 9 (fun _ -> sticker)
 
 let solved_cube =
-  [
-    side_one_sticker GREEN;
-    side_one_sticker ORANGE;
-    side_one_sticker BLUE;
-    side_one_sticker RED;
-    side_one_sticker YELLOW;
-    side_one_sticker WHITE;
-  ]
+  {
+    front = side_one_sticker GREEN;
+    right = side_one_sticker ORANGE;
+    back = side_one_sticker BLUE;
+    left = side_one_sticker RED;
+    top = side_one_sticker YELLOW;
+    bottom = side_one_sticker WHITE;
+  }
 
 let sticker_to_string = function
   | YELLOW -> "Y"
@@ -29,67 +39,54 @@ let side_to_string side =
   String.concat "" (List.mapi f side)
 
 let cube_to_string cube =
-  let side_string n = side_to_string (List.nth cube n) in
-  let front = side_string 0 in
-  let right = side_string 1 in
-  let back = side_string 2 in
-  let left = side_string 3 in
-  let top = side_string 4 in
-  let bottom = side_string 5 in
+  let front = side_to_string cube.front in
+  let right = side_to_string cube.right in
+  let back = side_to_string cube.back in
+  let left = side_to_string cube.left in
+  let top = side_to_string cube.top in
+  let bottom = side_to_string cube.bottom in
   String.concat "\n\n" [ top; front; right; back; left; bottom ]
 
 let rotate_side side clockwise =
   let new_side_indexes =
-    let indexes = [2; 5; 8; 1; 4; 7; 0; 3; 6] in
+    let indexes = [ 2; 5; 8; 1; 4; 7; 0; 3; 6 ] in
     if clockwise then indexes else List.rev indexes
   in
   List.map (fun i -> List.nth side i) new_side_indexes
 
-let move_up_clockwise cube =
-  let side_map i side =
-    let sticker_map j sticker =
-      let new_stickers_side = List.nth cube (if i = 3 then 0 else i + 1) in
-      if j < 3 then List.nth new_stickers_side j else sticker
-    in
-    if i = 5 then side
-    else if i = 4 then rotate_side side true
-    else List.mapi sticker_map side
+let move_up cube clockwise =
+  let sticker_map new_stickers_side i sticker =
+    if i < 3 then List.nth new_stickers_side i else sticker
   in
-  List.mapi side_map cube
-
-let move_up_counter_clockwise cube =
-  let side_map i side =
-    let sticker_map j sticker =
-      let new_stickers_side = List.nth cube (if i = 0 then 3 else i - 1) in
-      if j < 3 then List.nth new_stickers_side j else sticker
-    in
-    if i = 5 then side
-    else if i = 4 then rotate_side side false
-    else List.mapi sticker_map side
-  in
-  List.mapi side_map cube
-
-let move_clockwise cube direction =
-  match direction with
-  | UP -> move_up_clockwise cube
-  | DOWN -> move_up_clockwise cube
-  | RIGHT -> move_up_clockwise cube
-  | LEFT -> move_up_clockwise cube
-  | FRONT -> move_up_clockwise cube
-  | BACK -> move_up_clockwise cube
-
-let move_counter_clockwise cube direction =
-  match direction with
-  | UP -> move_up_counter_clockwise cube
-  | DOWN -> move_up_counter_clockwise cube
-  | RIGHT -> move_up_counter_clockwise cube
-  | LEFT -> move_up_counter_clockwise cube
-  | FRONT -> move_up_counter_clockwise cube
-  | BACK -> move_up_counter_clockwise cube
+  {
+    bottom = cube.bottom;
+    top = rotate_side cube.top clockwise;
+    front =
+      List.mapi
+        (sticker_map (if clockwise then cube.right else cube.left))
+        cube.front;
+    right =
+      List.mapi
+        (sticker_map (if clockwise then cube.back else cube.front))
+        cube.right;
+    back =
+      List.mapi
+        (sticker_map (if clockwise then cube.left else cube.right))
+        cube.back;
+    left =
+      List.mapi
+        (sticker_map (if clockwise then cube.front else cube.back))
+        cube.left;
+  }
 
 let move cube direction clockwise =
-  if clockwise then move_clockwise cube direction
-  else move_counter_clockwise cube direction
+  match direction with
+  | UP -> move_up cube clockwise
+  | DOWN -> move_up cube clockwise
+  | RIGHT -> move_up cube clockwise
+  | LEFT -> move_up cube clockwise
+  | FRONT -> move_up cube clockwise
+  | BACK -> move_up cube clockwise
 
 let cube = solved_cube
 let cube2 = move cube UP true
