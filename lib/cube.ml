cfe7048ceb2b75b9a91fb9284c9f8b61be663859
Mauritz <mauritz.sjodin3@gmail.com>
2024-09-14 07:26:42 +0200

    fix: make all clockwise moves correct

--- lib/cube.ml
+++ lib/cube.ml
@@ -54,6 +54,16 @@ let rotate_side side clockwise =
   in
   List.map (fun i -> List.nth side i) new_side_indexes
 
+module IntMap = Map.Make (Int)
+
+let replace_mapped mapped_indexes lst new_lst =
+  let map = IntMap.of_list mapped_indexes in
+  let f i x =
+    let mapped_index = IntMap.find_opt i map in
+    match mapped_index with Some j -> List.nth new_lst j | None -> x
+  in
+  List.mapi f lst
+
 let replace_at_indexes indexes lst new_lst =
   let f i x = if List.mem i indexes then List.nth new_lst i else x in
   List.mapi f lst
@@ -85,55 +95,99 @@ let move_down cube clockwise =
   }
 
 let move_right cube clockwise =
-  let replace_func = replace_at_indexes [ 2; 5; 8 ] in
   {
     left = cube.left;
     right = rotate_side cube.right clockwise;
     front =
-      replace_func cube.front (if clockwise then cube.bottom else cube.top);
-    top = replace_func cube.top (if clockwise then cube.front else cube.back);
-    back = replace_func cube.back (if clockwise then cube.top else cube.bottom);
+      replace_at_indexes [ 2; 5; 8 ] cube.front
+        (if clockwise then cube.bottom else cube.top);
+    top =
+      replace_at_indexes [ 2; 5; 8 ] cube.top
+        (if clockwise then cube.front else cube.back);
+    back =
+      replace_mapped
+        [ (0, 8); (3, 5); (6, 2) ]
+        cube.back
+        (if clockwise then cube.top else cube.bottom);
     bottom =
-      replace_func cube.bottom (if clockwise then cube.back else cube.front);
+      replace_mapped
+        [ (8, 0); (5, 3); (2, 6) ]
+        cube.bottom
+        (if clockwise then cube.back else cube.front);
   }
 
 let move_left cube clockwise =
-  let replace_func = replace_at_indexes [ 0; 3; 6 ] in
   {
     right = cube.right;
     left = rotate_side cube.left clockwise;
     front =
-      replace_func cube.front (if clockwise then cube.top else cube.bottom);
-    top = replace_func cube.top (if clockwise then cube.back else cube.front);
-    back = replace_func cube.back (if clockwise then cube.bottom else cube.top);
+      replace_at_indexes [ 0; 3; 6 ] cube.front
+        (if clockwise then cube.top else cube.bottom);
+    top =
+      replace_mapped
+        [ (0, 8); (3, 5); (6, 2) ]
+        cube.top
+        (if clockwise then cube.back else cube.front);
+    back =
+      replace_mapped
+        [ (8, 0); (5, 3); (2, 6) ]
+        cube.back
+        (if clockwise then cube.bottom else cube.top);
     bottom =
-      replace_func cube.bottom (if clockwise then cube.front else cube.back);
+      replace_at_indexes [ 0; 3; 6 ] cube.bottom
+        (if clockwise then cube.front else cube.back);
   }
 
 let move_front cube clockwise =
-  let replace_func = replace_at_indexes [ 0; 3; 6 ] in
   {
     back = cube.back;
     front = rotate_side cube.front clockwise;
     right =
-      replace_func cube.right (if clockwise then cube.top else cube.bottom);
-    top = replace_func cube.top (if clockwise then cube.left else cube.right);
-    left = replace_func cube.left (if clockwise then cube.bottom else cube.top);
+      replace_mapped
+        [ (0, 6); (3, 7); (6, 8) ]
+        cube.right
+        (if clockwise then cube.top else cube.bottom);
+    top =
+      replace_mapped
+        [ (6, 8); (7, 5); (8, 2) ]
+        cube.top
+        (if clockwise then cube.left else cube.right);
+    left =
+      replace_mapped
+        [ (2, 0); (5, 1); (8, 2) ]
+        cube.left
+        (if clockwise then cube.bottom else cube.top);
     bottom =
-      replace_func cube.bottom (if clockwise then cube.right else cube.left);
+      replace_mapped
+        [ (0, 6); (1, 3); (2, 0) ]
+        cube.bottom
+        (if clockwise then cube.right else cube.left);
   }
 
 let move_back cube clockwise =
-  let replace_func = replace_at_indexes [ 0; 1; 2 ] in
   {
     front = cube.front;
     back = rotate_side cube.back clockwise;
     right =
-      replace_func cube.right (if clockwise then cube.bottom else cube.top);
-    top = replace_func cube.top (if clockwise then cube.right else cube.left);
-    left = replace_func cube.left (if clockwise then cube.top else cube.bottom);
+      replace_mapped
+        [ (2, 8); (5, 7); (8, 6) ]
+        cube.right
+        (if clockwise then cube.bottom else cube.top);
+    top =
+      replace_mapped
+        [ (0, 2); (1, 5); (2, 8) ]
+        cube.top
+        (if clockwise then cube.right else cube.left);
+    left =
+      replace_mapped
+        [ (0, 2); (3, 1); (6, 0) ]
+        cube.left
+        (if clockwise then cube.top else cube.bottom);
     bottom =
-      replace_func cube.bottom (if clockwise then cube.left else cube.right);
+      replace_mapped
+        [ (6, 0); (7, 3); (8, 6) ]
+        cube.bottom
+        (if clockwise then cube.left else cube.right);
   }
 
 let move cube direction clockwise =
