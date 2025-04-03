cac9d299316b640ebf3f32d2168e7f103b86b5de
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-03 17:46:50 +0200

    refactor: extract duplicated logic

--- lib/solve.ml
+++ lib/solve.ml
@@ -1,67 +1,64 @@
 open Cube
 open Move
 
-let rec solve_cross cube =
+let cross_is_solved cube cross_color = 
+  let cross_edges = [
+    cube.top_face.fst.snd;
+    cube.top_face.snd.fst;
+    cube.top_face.snd.snd;
+    cube.top_face.snd.trd;
+    cube.top_face.trd.snd
+  ] in
+  let is_cross_color sticker = sticker == cross_color in
+  List.for_all is_cross_color cross_edges
+
+let rec solve_cross cube = 
   let cross_color = WHITE in
-  
+  if cross_is_solved cube cross_color then cube else
+  let cube_after_move = 
   if cube.bottom_face.fst.snd == cross_color then 
     let move = { layer = FRONT; clockwise = true } in
     let cube2 = make_move cube move in
-    let cube3 = make_move cube2 move in
-    solve_cross cube3
+    make_move cube2 move
   else if cube.bottom_face.snd.fst == cross_color then 
     let move = { layer = LEFT; clockwise = true } in
     let cube2 = make_move cube move in
-    let cube3 = make_move cube2 move in
-    solve_cross cube3
+    make_move cube2 move
   else if cube.bottom_face.snd.trd == cross_color then 
     let move = { layer = RIGHT; clockwise = true } in
     let cube2 = make_move cube move in
-    let cube3 = make_move cube2 move in
-    solve_cross cube3
+    make_move cube2 move
   else if cube.bottom_face.trd.snd == cross_color then 
     let move = { layer = BACK; clockwise = true } in
     let cube2 = make_move cube move in
-    let cube3 = make_move cube2 move in
-    solve_cross cube3
+    make_move cube2 move
 
   else if cube.middle_layer.front.fst == cross_color then
-    let cube2 = make_move cube { layer = LEFT; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = LEFT; clockwise = true }
   else if cube.middle_layer.front.trd == cross_color then
-    let cube2 = make_move cube { layer = RIGHT; clockwise = false } in
-    solve_cross cube2
+    make_move cube { layer = RIGHT; clockwise = false }
   else if cube.middle_layer.back.fst == cross_color then
-    let cube2 = make_move cube { layer = RIGHT; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = RIGHT; clockwise = true }
   else if cube.middle_layer.back.trd == cross_color then
-    let cube2 = make_move cube { layer = LEFT; clockwise = false } in
-    solve_cross cube2
+    make_move cube { layer = LEFT; clockwise = false }
   else if cube.middle_layer.right.fst == cross_color then
-    let cube2 = make_move cube { layer = FRONT; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = FRONT; clockwise = true }
   else if cube.middle_layer.right.trd == cross_color then
-    let cube2 = make_move cube { layer = BACK; clockwise = false } in
-    solve_cross cube2
+    make_move cube { layer = BACK; clockwise = false }
   else if cube.middle_layer.left.fst == cross_color then
-    let cube2 = make_move cube { layer = BACK; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = BACK; clockwise = true }
   else if cube.middle_layer.left.trd == cross_color then
-    let cube2 = make_move cube { layer = FRONT; clockwise = false } in
-    solve_cross cube2
+    make_move cube { layer = FRONT; clockwise = false }
 
   else if cube.top_layer.front.snd == cross_color || cube.bottom_layer.front.snd == cross_color then
-    let cube2 = make_move cube { layer = FRONT; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = FRONT; clockwise = true }
   else if cube.top_layer.back.snd == cross_color || cube.bottom_layer.back.snd == cross_color then
-    let cube2 = make_move cube { layer = BACK; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = BACK; clockwise = true }
   else if cube.top_layer.right.snd == cross_color || cube.bottom_layer.right.snd == cross_color then
-    let cube2 = make_move cube { layer = RIGHT; clockwise = true } in
-    solve_cross cube2
+    make_move cube { layer = RIGHT; clockwise = true }
   else if cube.top_layer.left.snd == cross_color || cube.bottom_layer.left.snd == cross_color then
-    let cube2 = make_move cube { layer = LEFT; clockwise = true } in
-    solve_cross cube2
-    
+    make_move cube { layer = LEFT; clockwise = true }
   else 
-    cube
+    failwith "unable to solve cross"
+  in
+  solve_cross cube_after_move
