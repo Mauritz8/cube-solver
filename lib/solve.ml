ed52519e00a81f8592d5a250c19a2a8af465ba44
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-03 17:17:21 +0200

    wip: solve cross edges in bottom face

--- lib/solve.ml
+++ lib/solve.ml
@@ -1,1 +1,27 @@
-let solve_cross cube = cube
+open Cube
+open Move
+
+let rec solve_cross cube =
+  let cross_color = WHITE in
+  if cube.bottom_face.fst.snd == cross_color then 
+    let move = { layer = FRONT; clockwise = true } in
+    let cube2 = Move.make_move cube move in
+    let cube3 = Move.make_move cube2 move in
+    solve_cross cube3
+  else if cube.bottom_face.snd.fst == cross_color then 
+    let move = { layer = LEFT; clockwise = true } in
+    let cube2 = Move.make_move cube move in
+    let cube3 = Move.make_move cube2 move in
+    solve_cross cube3
+  else if cube.bottom_face.snd.trd == cross_color then 
+    let move = { layer = RIGHT; clockwise = true } in
+    let cube2 = Move.make_move cube move in
+    let cube3 = Move.make_move cube2 move in
+    solve_cross cube3
+  else if cube.bottom_face.trd.snd == cross_color then 
+    let move = { layer = BACK; clockwise = true } in
+    let cube2 = Move.make_move cube move in
+    let cube3 = Move.make_move cube2 move in
+    solve_cross cube3
+  else 
+    cube
