2bee39e73ae3efe0a10da91acf9a139e283f69ff
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-05 16:36:46 +0200

    feat: raise failure if solve is taking too many moves

--- lib/solve.ml
+++ lib/solve.ml
@@ -1,5 +1,4 @@
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
-
 open Cube
 open Move
 
@@ -24,68 +23,71 @@ let solve_cross_next_moves cube cross_color =
     let center = cube.middle_layer.front.snd in
     if edge == center then
       let move = { layer = FRONT; clockwise = true } in
-      [move; move]
-    else [{ layer = BOTTOM; clockwise = true }]
+      [ move; move ]
+    else [ { layer = BOTTOM; clockwise = true } ]
   else if cube.bottom_face.snd.fst == cross_color then
     let edge = cube.bottom_layer.left.snd in
     let center = cube.middle_layer.left.snd in
     if edge == center then
       let move = { layer = LEFT; clockwise = true } in
-      [move; move]
-    else [{ layer = BOTTOM; clockwise = true }]
+      [ move; move ]
+    else [ { layer = BOTTOM; clockwise = true } ]
   else if cube.bottom_face.snd.trd == cross_color then
     let edge = cube.bottom_layer.right.snd in
     let center = cube.middle_layer.right.snd in
     if edge == center then
       let move = { layer = RIGHT; clockwise = true } in
-      [move; move]
-    else [{ layer = BOTTOM; clockwise = true }]
+      [ move; move ]
+    else [ { layer = BOTTOM; clockwise = true } ]
   else if cube.bottom_face.trd.snd == cross_color then
     let edge = cube.bottom_layer.back.snd in
     let center = cube.middle_layer.back.snd in
     if edge == center then
       let move = { layer = BACK; clockwise = true } in
-      [move; move]
-    else [{ layer = BOTTOM; clockwise = true }]
+      [ move; move ]
+    else [ { layer = BOTTOM; clockwise = true } ]
   else if cube.middle_layer.front.fst == cross_color then
-    [{ layer = LEFT; clockwise = true }]
+    [ { layer = LEFT; clockwise = true } ]
   else if cube.middle_layer.front.trd == cross_color then
-    [{ layer = RIGHT; clockwise = false }]
+    [ { layer = RIGHT; clockwise = false } ]
   else if cube.middle_layer.back.fst == cross_color then
-    [{ layer = RIGHT; clockwise = true }]
+    [ { layer = RIGHT; clockwise = true } ]
   else if cube.middle_layer.back.trd == cross_color then
-    [{ layer = LEFT; clockwise = false }]
+    [ { layer = LEFT; clockwise = false } ]
   else if cube.middle_layer.right.fst == cross_color then
-    [{ layer = FRONT; clockwise = true }]
+    [ { layer = FRONT; clockwise = true } ]
   else if cube.middle_layer.right.trd == cross_color then
-    [{ layer = BACK; clockwise = false }]
+    [ { layer = BACK; clockwise = false } ]
   else if cube.middle_layer.left.fst == cross_color then
-    [{ layer = BACK; clockwise = true }]
+    [ { layer = BACK; clockwise = true } ]
   else if cube.middle_layer.left.trd == cross_color then
-    [{ layer = FRONT; clockwise = false }]
+    [ { layer = FRONT; clockwise = false } ]
   else if
     cube.top_layer.front.snd == cross_color
     || cube.bottom_layer.front.snd == cross_color
-  then [{ layer = FRONT; clockwise = true }]
+  then [ { layer = FRONT; clockwise = true } ]
   else if
     cube.top_layer.back.snd == cross_color
     || cube.bottom_layer.back.snd == cross_color
-  then [{ layer = BACK; clockwise = true }]
+  then [ { layer = BACK; clockwise = true } ]
   else if
     cube.top_layer.right.snd == cross_color
     || cube.bottom_layer.right.snd == cross_color
-  then [{ layer = RIGHT; clockwise = true }]
+  then [ { layer = RIGHT; clockwise = true } ]
   else if
     cube.top_layer.left.snd == cross_color
     || cube.bottom_layer.left.snd == cross_color
-  then [{ layer = LEFT; clockwise = true }]
-  else failwith "unable to solve cross"
+  then [ { layer = LEFT; clockwise = true } ]
+  else failwith "Unable to solve cross: didn't find an edge"
 
 let solve_cross cube =
   let cross_color = WHITE in
   let rec solve_cross_helper cube moves =
-    if cross_is_solved cube cross_color then
-      { cube = cube; moves = List.map move_to_notation moves }
+    if List.length moves > 100 then
+      failwith
+        "Unable to solve cross: didn't find solution with less than 100 moves"
+    else if cross_is_solved cube cross_color then
+      { cube; moves = List.map move_to_notation moves }
     else
       let next_moves = solve_cross_next_moves cube cross_color in
       let cube_apply_move cube move = make_move cube move in
